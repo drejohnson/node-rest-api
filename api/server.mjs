9bc119ed060efc4b665171f78e00c4dea6b8eb1c
DeAndre Johnson <dre@phreshr.com>
2019-12-06 04:50:56 +0800

    fix error

--- api/server.mjs
+++ api/server.mjs
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use("/api/user", authRoute);
 app.use("/api/posts", postsRoute);
 
-server.use("/", (req, res) =>
+app.use("/", (req, res) =>
   res.send(`
     <h2>Node REST API with Auth</h2>
   `)
