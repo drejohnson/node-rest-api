3a43259b3df255a4a3dea17425e8d56223bdb047
DeAndre Johnson <dre@phreshr.com>
2019-12-06 06:06:29 +0800

    add helmet

--- api/server.mjs
+++ api/server.mjs
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
 
 import authRoute from "../routes/auth.mjs";
 import postsRoute from "../routes/posts.mjs";
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use("/api/user", authRoute);
